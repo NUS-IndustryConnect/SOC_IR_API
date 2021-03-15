6f2a7e83f57b0fe03dadfd421277170dd6f465ae
EkamSinghPandher <pandher.ekam@gmail.com>
2021-03-15 23:12:23 +0800

    Updates to the code and ADFS

---
+++ SOC_IR/Dtos/CompanyUser/UpdateCompanyUserDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SOC_IR.Dtos.CompanyUser
+{
+    public class UpdateCompanyUserDto
+    {
+        public string companyUserId { get; set; }
+        public string companyName { get; set; }
+        public string email { get; set; }
+    }
+}
--- SOC_IR/Services/CompanyUserService/CompanyUserService.cs
+++ SOC_IR/Services/CompanyUserService/CompanyUserService.cs
@@ -16,10 +16,10 @@ namespace SOC_IR.Services.CompanyUserService
         {
             _context = context;
         }
-        async Task<ServiceResponse<CompanyUserDto>> ICompanyUserService.CreateCompanyUser(CreateCompanyUserDto companyUserDto)
+        async Task<ServiceResponse<List<CompanyUserDto>>> ICompanyUserService.CreateCompanyUser(CreateCompanyUserDto companyUserDto)
         {
 
-            ServiceResponse<CompanyUserDto> response = new ServiceResponse<CompanyUserDto>();
+            ServiceResponse<List<CompanyUserDto>> response = new ServiceResponse<List<CompanyUserDto>>();
             Company company = await _context.Companies.FirstOrDefaultAsync(a => a.companyId == companyUserDto.companyId);
             CompanyUser user = await _context.CompanyUsers.FirstOrDefaultAsync(a => a.email == companyUserDto.email);
             if (user != null)
@@ -41,8 +41,8 @@ namespace SOC_IR.Services.CompanyUserService
             CompanyUser newUser = new CompanyUser(finalString, company.companyName, companyUserDto.companyId, companyUserDto.email, lastLoggedIn, true);
             await _context.CompanyUsers.AddAsync(newUser);
             await _context.SaveChangesAsync();
-            CompanyUserDto newUserDto = new CompanyUserDto(newUser.companyUserId, newUser.companyId, newUser.companyName, newUser.email, newUser.lastLoggedIn, newUser.isActive);
-            response.Data = newUserDto;
+            List<CompanyUserDto> newList = await _context.CompanyUsers.Select(a => new CompanyUserDto(a.companyUserId, a.companyId, a.companyName, a.email, a.lastLoggedIn, a.isActive)).ToListAsync();
+            response.Data = newList;
             return response;
         }
 
@@ -135,6 +135,25 @@ namespace SOC_IR.Services.CompanyUserService
             await _context.SaveChangesAsync();
             CompanyUserDto userDto = new CompanyUserDto(user.companyUserId, user.companyId, user.companyName, user.email, user.lastLoggedIn, user.isActive);
             response.Data = userDto;
+            return response;
+        }
+
+        public async Task<ServiceResponse<List<CompanyUserDto>>> UpdateCompanyUser(UpdateCompanyUserDto updateDto)
+        {
+            ServiceResponse<List<CompanyUserDto>> response = new ServiceResponse<List<CompanyUserDto>>();
+            CompanyUser user = await _context.CompanyUsers.FirstOrDefaultAsync(a => a.companyUserId == updateDto.companyUserId);
+            if (user == null)
+            {
+                response.Success = false;
+                response.Message = "The company user you wish to update does not exist";
+                return response;
+            }
+            user.companyName = updateDto.companyName;
+            user.email = updateDto.email;
+            _context.CompanyUsers.Update(user);
+            await _context.SaveChangesAsync();
+            List<CompanyUserDto> newList = await _context.CompanyUsers.Where(a => a.companyId == companyId).Select(a => new CompanyUserDto(a.companyUserId, a.companyId, a.companyName, a.email, a.lastLoggedIn, a.isActive)).ToListAsync();
+            response.Data = newList;
             return response;
         }
     }
